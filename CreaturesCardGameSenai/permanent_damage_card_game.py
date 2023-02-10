2426ba90ca7e7850e638186d5567bc4e5eb8d167
John Lucas <joohnzay@gmail.com>
2023-02-10 12:07:43 -0300

    Update

--- CreaturesCardGameSenai/permanent_damage_card_game.py
+++ CreaturesCardGameSenai/permanent_damage_card_game.py
@@ -5,7 +5,3 @@ from creature import Creature
 class PermanentDamageCardGame(CardGame):
     def hit(self, attacker: Creature, defender: Creature):
         defender.health -= attacker.attack
-        
-        print('-' * 15, 'Permanent damage hit', '-' * 15)
-        print(f'{attacker.name} causou {attacker.attack} de dano...\n')
-        print(f"{defender.name}'s HP: {defender.health}")
